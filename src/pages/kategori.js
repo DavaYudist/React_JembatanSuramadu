bf347a0c6f0ef1402034a0e26d38b3fc2a2528fe
DAVANA YUDIST <davayudist@gmail.com>
2025-10-29 16:06:50 +0700

    update repository

---
+++ src/pages/kategori.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function kategori() {
+  return (
+    <div>
+      <h1>Selamat datang di laman kategori</h1>
+    </div>
+  );
+}
+
+export default kategori;
---
+++ src/pages/produk.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function produk() {
+  return (
+    <div>
+      <h1>Selamat datang di laman produk</h1>
+    </div>
+  );
+}
+
+export default produk;
